2af0ad31e3bf7ea75baac4f158603e70d30930ca
Plinio Naves <pliniopjn@hotmail.com>
2019-09-05 18:58:45 -0300

    Implementando primeira Mutation

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -4,13 +4,25 @@ import { GraphQLServer } from 'graphql-yoga'
 const typeDefs = resolve(__dirname, 'schema.graphql')
 
 const USERS = [
-  { id: 1, name: 'Tony Stark' },
-  { id: 2, name: 'Spider Man' }
+  { id: 1, name: 'Tony Stark', email: 'tony@avengers.com' },
+  { id: 2, name: 'Spider Man', email: 'spider@avengers.com' }
 ]
 
 const resolvers = {
   Query: {
     users: () => USERS
+  },
+  Mutation: {
+    createUser: (parent, args, ctx, info) => {
+      console.log('args: ', args)
+      const { data } = args
+      const user = {
+        ...data,
+        id: USERS.length + 1
+      }
+      USERS.push(user)
+      return user
+    }
   }
 }
 
