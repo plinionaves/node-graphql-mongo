c9d100155bed1e0a02657078c7a44334bedadf4c
Plinio Naves <pliniopjn@hotmail.com>
2019-09-05 16:04:15 -0300

    Configurando o GraphQL Yoga

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello Node, TypeScript, Nodemon and Sucrase...')
+import server from './server'
+
+const { PORT: port = 4000 } = process.env
+
+const main = async (): Promise<void> => {
+  await server.start({ port })
+  console.log(`Listening at port ${port}...`)
+}
+
+main().catch(console.log)
