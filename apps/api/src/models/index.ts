9e772d11db75155d83a7bdc6adec48d780cfd61d
Plinio Naves <pliniopjn@hotmail.com>
2019-09-11 17:45:13 -0300

    Configurando carregamento dos Models

--- apps/api/src/models/index.ts
+++ apps/api/src/models/index.ts
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import { resolve } from 'path'
+import { readdirSync } from 'fs'
 
 const { MONGO_URI } = process.env
 
@@ -10,4 +12,17 @@ const connect = (): Promise<typeof mongoose> =>
     useUnifiedTopology: true,
   })
 
-export { connect }
+const models = Object.create({})
+
+readdirSync(__dirname)
+  .filter(fileName => !fileName.includes('index'))
+  .forEach(fileName => {
+    const model = require(resolve(__dirname, fileName)).default
+    const modelName = fileName
+      .split('.')
+      .shift()
+      .replace('Model', '')
+    models[modelName] = model
+  })
+
+export { connect, models }
