e11118f876f24e8ea974429de7e1e22c4d9fc06b
Plinio Naves <pliniopjn@hotmail.com>
2019-09-13 12:44:05 -0300

    Implementando OrderModel e tipos

---
+++ apps/api/src/types/orderTypes.ts
@@ -0,0 +1,24 @@
+import { Document, Schema } from 'mongoose'
+import { User } from '.'
+
+export enum OrderStatus {
+  WAITING_PAYMENT,
+  IN_QUEUE,
+  PREPARING,
+  READY,
+  ON_THE_WAY,
+  DELIVERED,
+}
+
+export interface Order {
+  _id: Schema.Types.ObjectId
+  user: User
+  total: number
+  status: OrderStatus
+  createdAt: string
+  updatedAt: string
+}
+
+export interface OrderDocument extends Order, Document {
+  _id: Schema.Types.ObjectId
+}
