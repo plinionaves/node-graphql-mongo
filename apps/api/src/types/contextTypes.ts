04935e2e1bd15fce21e68fc74efa7e4e7cd7fe66
Plinio Naves <pliniopjn@hotmail.com>
2019-10-04 17:24:47 -0300

    Configurando PubSub no Context dos Resolvers

--- apps/api/src/types/contextTypes.ts
+++ apps/api/src/types/contextTypes.ts
@@ -1,7 +1,9 @@
+import { PubSub } from 'graphql-yoga'
 import { ContextParameters } from 'graphql-yoga/dist/types'
 import { AuthUser, Models } from '.'
 
 export interface Context extends ContextParameters {
   authUser: AuthUser
   db: Models
+  pubsub: PubSub
 }
