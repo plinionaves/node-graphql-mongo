ca3b1c0009007123213b7445025d07a63c7b1963
Plinio Naves <pliniopjn@hotmail.com>
2019-10-08 10:43:53 -0300

    Filtrando dados publicados de acordo com usuário autenticado

--- apps/api/src/resolvers/Subscription.ts
+++ apps/api/src/resolvers/Subscription.ts
@@ -1,16 +1,35 @@
-import { Order, SubscriptionResolver } from '../types'
+import { withFilter } from 'graphql-yoga'
+import { Types } from 'mongoose'
+import {
+  Order,
+  Resolver,
+  SubscriptionArgs,
+  SubscriptionPayload,
+  SubscriptionResolver,
+  UserRole,
+} from '../types'
+
+const orderSubscribeFn: Resolver<SubscriptionArgs> = (_, args, ctx) => {
+  const { mutationIn } = args.where
+  const { pubsub } = ctx
+  const channels = mutationIn.map(m => `ORDER_${m}`)
+  return pubsub.asyncIterator(channels)
+}
+
+const orderFilterFn: Resolver<SubscriptionArgs, SubscriptionPayload<Order>> = (
+  payload,
+  args,
+  ctx,
+) => {
+  const { _id, role } = ctx.authUser
+  return role === UserRole.ADMIN
+    ? true
+    : (payload.node.user as Types.ObjectId).equals(_id)
+}
 
 const order: SubscriptionResolver<Order> = {
-  subscribe: (_, args, ctx) => {
-    const { mutationIn } = args.where
-    const { pubsub } = ctx
-    const channels = mutationIn.map(m => `ORDER_${m}`)
-    return pubsub.asyncIterator(channels)
-  },
-  resolve: payload => {
-    console.log('Payload: ', payload)
-    return payload
-  },
+  subscribe: withFilter(orderSubscribeFn, orderFilterFn),
+  resolve: payload => payload,
 }
 
 export default {
