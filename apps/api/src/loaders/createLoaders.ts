a45f5b902e477b743670a87802f713ed169a5168
Plinio Naves <pliniopjn@hotmail.com>
2019-10-10 10:36:58 -0300

    Implementando Batch Load Function

--- apps/api/src/loaders/createLoaders.ts
+++ apps/api/src/loaders/createLoaders.ts
@@ -1,7 +1,30 @@
 import DataLoader from 'dataloader'
-import { Document, Types } from 'mongoose'
+import { Document, Model, Types } from 'mongoose'
 import { models as db } from '../models'
 import { DataLoaders, DataLoaderParam, Models } from '../types'
+
+const batchLoadFn = async (
+  model: Model<Document>,
+  params: DataLoaderParam[],
+): Promise<Document[]> => {
+  const ids = params.map(param => param.key)
+  const { select } = params[0]
+
+  const documents = await model
+    .find({ _id: { $in: ids } })
+    .select(select)
+    .exec()
+
+  const documentsMap = documents.reduce(
+    (prev, document) => ({
+      ...prev,
+      [document._id.toHexString()]: document,
+    }),
+    {} as { [key: string]: Document },
+  )
+
+  return ids.map(id => documentsMap[id.toHexString()])
+}
 
 const createLoaders = (modelsNames: (keyof Models)[]): DataLoaders =>
   modelsNames.reduce(
