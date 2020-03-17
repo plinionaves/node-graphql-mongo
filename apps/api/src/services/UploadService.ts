0b02256d18255217a45f00d7fd05e4432a262747
Plinio Naves <pliniopjn@hotmail.com>
2020-03-16 22:47:02 -0300

    UploadService: processando upload em dev usando Streams

--- apps/api/src/services/UploadService.ts
+++ apps/api/src/services/UploadService.ts
@@ -2,9 +2,14 @@ import fs from 'fs'
 import { resolve } from 'path'
 import { promisify } from 'util'
 import mkdirpCb from 'mkdirp'
+import { FileUpload } from 'graphql-upload'
+import { Types } from 'mongoose'
+
+import { File, FileData } from '../types'
 
 const exists = promisify(fs.access)
 const mkdirp = promisify(mkdirpCb)
+const { CDN_ENDPOINT } = process.env
 
 export class UploadService {
   private uploadDir: string
@@ -25,5 +30,44 @@ export class UploadService {
     } catch (error) {
       await mkdirp(this.uploadDir)
     }
+  }
+
+  async processUpload(upload: FileUpload): Promise<File> {
+    const {
+      createReadStream,
+      encoding,
+      mimetype,
+      filename: originalname,
+    } = upload
+
+    const _id = new Types.ObjectId()
+    const filename = `${_id}-${originalname}`
+    const stream = createReadStream()
+    const path = `static/${filename}`
+
+    const fileData = {
+      _id,
+      filename,
+      encoding,
+      mimetype,
+      path,
+      stream,
+    }
+
+    return this.processUploadDevelopment(fileData)
+  }
+
+  private processUploadDevelopment(fileData: FileData): Promise<File> {
+    const { filename, path, stream, ...rest } = fileData
+
+    const url = `${CDN_ENDPOINT}/${path}`
+    const fullPath = `${this.uploadDir}/${filename}`
+
+    return new Promise((resolve, reject): void => {
+      stream
+        .pipe(fs.createWriteStream(fullPath))
+        .on('finish', () => resolve({ ...rest, filename, url, path }))
+        .on('error', reject)
+    })
   }
 }
--- apps/api/src/types/fileTypes.ts
+++ apps/api/src/types/fileTypes.ts
@@ -1,4 +1,19 @@
 import { FileUpload } from 'graphql-upload'
+import { Types } from 'mongoose'
+import { ReadStream } from 'fs'
+
+export interface File {
+  _id: Types.ObjectId
+  filename: string
+  path: string
+  mimetype: string
+  encoding: string
+  url: string
+}
+
+export interface FileData extends Omit<File, 'url'> {
+  stream: ReadStream
+}
 
 export interface UploadCreateInput {
   file: FileUpload
