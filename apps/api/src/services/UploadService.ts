fbe0d8699a6222fe9c4508921599a02464159455
Plinio Naves <pliniopjn@hotmail.com>
2020-09-25 10:29:59 -0300

    Instalando e configurando o aws-sdk

--- apps/api/src/services/UploadService.ts
+++ apps/api/src/services/UploadService.ts
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 import { promisify } from 'util'
 import { FileUpload } from 'graphql-upload'
 import { Types } from 'mongoose'
+import aws from 'aws-sdk'
 import mkdirpCb from 'mkdirp'
 import sharp from 'sharp'
 
@@ -10,8 +11,17 @@ import { File, FileData } from '../types'
 
 const exists = promisify(fs.access)
 const mkdirp = promisify(mkdirpCb)
+const {
+  NODE_ENV,
+  DO_SPACES_ACCESS_KEY,
+  DO_SPACES_BUCKET,
+  DO_SPACES_ENDPOINT,
+  DO_SPACES_SECRET_KEY,
+} = process.env
 
 export class UploadService {
+  private isProduction: boolean
+  private S3: aws.S3
   private uploadDir: string
 
   constructor() {
@@ -19,7 +29,17 @@ export class UploadService {
   }
 
   private init(): void {
-    this.createUploadDir()
+    this.isProduction = NODE_ENV === 'production'
+
+    if (this.isProduction) {
+      this.S3 = new aws.S3({
+        accessKeyId: DO_SPACES_ACCESS_KEY,
+        secretAccessKey: DO_SPACES_SECRET_KEY,
+        endpoint: DO_SPACES_ENDPOINT,
+      })
+    } else {
+      this.createUploadDir()
+    }
   }
 
   private async createUploadDir(): Promise<void> {
@@ -56,7 +76,9 @@ export class UploadService {
       stream,
     }
 
-    return this.processUploadDevelopment(fileData)
+    return this.isProduction
+      ? this.processUploadProduction(fileData)
+      : this.processUploadDevelopment(fileData)
   }
 
   private processUploadDevelopment(fileData: FileData): Promise<File> {
@@ -70,5 +92,27 @@ export class UploadService {
         .on('finish', () => resolve({ ...rest, filename, path }))
         .on('error', reject)
     })
+  }
+
+  private async processUploadProduction(fileData: FileData): Promise<File> {
+    const { _id, encoding, filename, mimetype, path, stream } = fileData
+
+    const S3Upload = await this.S3.upload({
+      ACL: 'public-read',
+      Bucket: DO_SPACES_BUCKET,
+      Key: path,
+      Body: stream,
+      ContentType: mimetype,
+      ContentEncoding: encoding,
+    }).promise()
+
+    return {
+      _id,
+      encoding,
+      filename,
+      mimetype,
+      path,
+      url: S3Upload.Location,
+    }
   }
 }
