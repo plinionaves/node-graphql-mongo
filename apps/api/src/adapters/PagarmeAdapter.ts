1b9dd66f37488a5e7a2e4e81010e84bf6882142b
Plinio Naves <pliniopjn@hotmail.com>
2021-01-31 12:53:45 -0300

    PagarmeAdapter: tratando erros

--- apps/api/src/adapters/PagarmeAdapter.ts
+++ apps/api/src/adapters/PagarmeAdapter.ts
@@ -10,6 +10,7 @@ import pagarme, {
   TransactionStatus,
 } from 'pagarme'
 
+import { CustomError } from '../errors'
 import {
   PaymentGateway,
   PaymentMethod,
@@ -18,86 +19,138 @@ import {
   PaymentTransactionStatus,
 } from '../interfaces/PaymentGateway'
 
+interface PagarmeError {
+  message: string
+  parameter_name: string
+  type:
+    | 'action_forbidden	'
+    | 'internal_error'
+    | 'invalid_parameter'
+    | 'not_found'
+}
+
+interface PagarmeErrorPayload {
+  name: string
+  response: {
+    errors: PagarmeError[]
+    method: 'DELETE' | 'GET' | 'POST' | 'PUT'
+    status: number
+    url: string
+  }
+}
+
 export class PagarmeAdapter implements PaymentGateway {
   async transaction(
     data: PaymentTransactionRequest,
   ): Promise<PaymentTransactionResponse> {
-    const country = data.address.country.toLowerCase() as Country
-    const address: Address = {
-      city: data.address.city,
-      complementary: data.address.complement,
-      country,
-      state: data.address.state,
-      street: data.address.street,
-      street_number: data.address.number + '',
-      zipcode: data.address.zipcode,
-      neighborhood: data.address.neighborhood,
-    }
+    try {
+      const country = data.address.country.toLowerCase() as Country
+      const address: Address = {
+        city: data.address.city,
+        complementary: data.address.complement,
+        country,
+        state: data.address.state,
+        street: data.address.street,
+        street_number: data.address.number + '',
+        zipcode: data.address.zipcode,
+        neighborhood: data.address.neighborhood,
+      }
 
-    let customerType: CustomerType
-    let documentNumber: string
-    let documentType: DocumentType
+      let customerType: CustomerType
+      let documentNumber: string
+      let documentType: DocumentType
 
-    if (data.customer.cpf) {
-      customerType = 'individual'
-      documentNumber = data.customer.cpf
-      documentType = 'cpf'
-    } else if (data.customer.cnpj) {
-      customerType = 'corporation'
-      documentNumber = data.customer.cnpj
-      documentType = 'cnpj'
-    } else {
-      customerType = 'other'
-      documentType = 'other'
-    }
+      if (data.customer.cpf) {
+        customerType = 'individual'
+        documentNumber = data.customer.cpf
+        documentType = 'cpf'
+      } else if (data.customer.cnpj) {
+        customerType = 'corporation'
+        documentNumber = data.customer.cnpj
+        documentType = 'cnpj'
+      } else {
+        customerType = 'other'
+        documentType = 'other'
+      }
 
-    documentNumber = this.replaceDocumentNumber(documentNumber)
+      documentNumber = this.replaceDocumentNumber(documentNumber)
 
-    const registeredCustomer = await this.findCustomer(
-      data.customer.email,
-      documentNumber,
-    )
+      const registeredCustomer = await this.findCustomer(
+        data.customer.email,
+        documentNumber,
+      )
 
-    const customer = registeredCustomer
-      ? { id: registeredCustomer.id }
-      : {
-          country,
-          documents: [{ type: documentType, number: documentNumber }],
-          email: data.customer.email,
-          external_id: data.customer.id + '',
+      const customer = registeredCustomer
+        ? { id: registeredCustomer.id }
+        : {
+            country,
+            documents: [{ type: documentType, number: documentNumber }],
+            email: data.customer.email,
+            external_id: data.customer.id + '',
+            name: data.customer.name,
+            phone_numbers: data.customer.phoneNumbers,
+            type: customerType,
+          }
+
+      const client = await this.getClient()
+      const transaction = await client.transactions.create({
+        amount: data.amount * 100,
+        billing: {
+          address,
           name: data.customer.name,
-          phone_numbers: data.customer.phoneNumbers,
-          type: customerType,
-        }
+        },
+        card_hash: data.cardHash,
+        customer,
+        items: data.items.map(item => ({
+          id: item.id + '',
+          quantity: item.quantity,
+          tangible: item.tangible,
+          title: item.name,
+          unit_price: item.price * 100,
+        })),
+        payment_method: this.getPaymentMethod(data.paymentMethod) as any,
+        shipping: {
+          address,
+          fee: data.shipping.fee * 100,
+          name: data.shipping.receiver,
+          delivery_date: format(data.shipping.deliveryDate, 'yyyy-MM-dd'),
+        },
+      })
 
-    const client = await this.getClient()
-    const transaction = await client.transactions.create({
-      amount: data.amount * 100,
-      billing: {
-        address,
-        name: data.customer.name,
-      },
-      card_hash: data.cardHash,
-      customer,
-      items: data.items.map(item => ({
-        id: item.id + '',
-        quantity: item.quantity,
-        tangible: item.tangible,
-        title: item.name,
-        unit_price: item.price * 100,
-      })),
-      payment_method: this.getPaymentMethod(data.paymentMethod) as any,
-      shipping: {
-        address,
-        fee: data.shipping.fee * 100,
-        name: data.shipping.receiver,
-        delivery_date: format(data.shipping.deliveryDate, 'yyyy-MM-dd'),
-      },
-    })
+      return {
+        id: transaction.id + '',
+        status: this.getTransactionStatus(transaction.status),
+      }
+    } catch (error) {
+      const pagarmeErrorPayload = error as PagarmeErrorPayload
+      throw this.processErrors(pagarmeErrorPayload.response.errors)
+    }
+  }
 
-    return {
-      id: transaction.id + '',
-      status: this.getTransactionStatus(transaction.status),
+  private processErrors(errors: PagarmeError[]) {
+    let error: CustomError | PagarmeError
+
+    for (const pagarmeError of errors) {
+      error = this.createError(pagarmeError)
+
+      if (error instanceof CustomError) {
+        break
+      }
+    }
+
+    return error
+  }
+
+  private createError(error: PagarmeError) {
+    switch (error.parameter_name) {
+      case 'card_hash':
+        return new CustomError('Invalid card hash!', 'INVALID_CARD_HASH_ERROR')
+
+      case 'card_id':
+        return new CustomError('Card id not found!', 'INVALID_CARD_ID_ERROR')
+
+      default:
+        return error
     }
   }
 
