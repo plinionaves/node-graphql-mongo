28fa0f5bf80d19db0002f91c36eeebfe8f000bf9
Plinio Naves <pliniopjn@hotmail.com>
2021-02-04 09:41:53 -0300

    PaymentModel: Schema GraphQL e Order resolver

--- apps/api/src/resolvers/Order.ts
+++ apps/api/src/resolvers/Order.ts
@@ -2,6 +2,13 @@ import { Types } from 'mongoose'
 import { Order, Resolver } from '../types'
 import { getFields } from '../utils'
 
+const payments: Resolver<any, Order> = (order, args, { db }, info) => {
+  const { Payment } = db
+  return Payment.find({
+    order: order._id,
+  }).select(getFields(info))
+}
+
 const user: Resolver<any, Order> = (order, args, { loaders }, info) => {
   const { userLoader } = loaders
   return userLoader.load({
@@ -11,5 +18,6 @@ const user: Resolver<any, Order> = (order, args, { loaders }, info) => {
 }
 
 export default {
+  payments,
   user,
 }
