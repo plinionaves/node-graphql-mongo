5e12f3469b43b9d8e24a76ddf6cf6283f1616d2e
Plinio Naves <pliniopjn@hotmail.com>
2021-02-16 22:59:50 -0300

    Criando rota no express para receber os Postbacks

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,10 +1,12 @@
 import { resolve } from 'path'
 import { GraphQLServer } from 'graphql-yoga'
+import bodyParser from 'body-parser'
 import express from 'express'
 
 import { context } from './config'
 import { AuthDirective } from './directives'
 import { catchErrorsMiddleware } from './middlewares'
+import { paymentsService } from './services'
 import resolvers from './resolvers'
 
 const typeDefs = resolve(__dirname, 'schema.graphql')
@@ -22,6 +24,20 @@ const server = new GraphQLServer({
 server.use(
   '/static',
   express.static(resolve(__dirname, '..', 'tmp', 'uploads')),
+)
+
+server.express.post(
+  '/webhooks/pagarme',
+  bodyParser.urlencoded(),
+  async (req, res) => {
+    const validated = await paymentsService.processWebhook(
+      req.headers as any,
+      req.body,
+    )
+    const status = validated ? 200 : 412
+
+    res.status(status).json({ ok: validated })
+  },
 )
 
 export default server
